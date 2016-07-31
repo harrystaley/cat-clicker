8ef14ed588bc84226ced7dae836b9080bba9b9bc
Harry Staley <staleyh@gmail.com>
2016-07-31 02:03:26 -0500

    FEAT: added cat object in separate file.

---
+++ script.js
@@ -0,0 +1,34 @@
+var cats = [
+		{
+			name: "Boots",
+			clickCount: 0,
+			imgUrl: "http://placekitten.com/200/300?image=1"
+		},
+		{
+			name: "Snickers",
+			clickCount: 0,
+			imgUrl: "http://placekitten.com/200/300?image=2"
+		},
+		{
+			name: "Fluffy",
+			clickCount: 0,
+			imgUrl: "http://placekitten.com/200/300?image=3"
+		},
+		{
+			name: "Snuggles",
+			clickCount: 0,
+			imgUrl: "http://placekitten.com/200/300?image=4"
+		},
+		{
+			name: "Mr. Bigglesworth",
+			clickCount: 0,
+			imgUrl: "http://placekitten.com/200/300?image=5"
+		}
+	];
+
+
+	// var elem = document.getElementById('catImg1');
+	// elem.addEventListener('click', function(){
+	//   clickCount++
+	//   document.getElementById("count").innerHTML = "Clicks: " + cat.clickCount;
+	// }, false);
